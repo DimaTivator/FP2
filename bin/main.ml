cf5a952602548be98a3515302c19dff60ac610f2
DimaTivator <andriianov164@gmail.com>
2024-10-09 13:20:02 +0300

    add: remove function

--- bin/main.ml
+++ bin/main.ml
@@ -2,7 +2,9 @@ open Rbset
 
 let () =
   let open Rbset in
-  let set = empty |> insert 10 |> insert 10 |> insert 20 |> insert 15 in
+  let set =
+    empty |> remove 20 |> insert 10 |> insert 10 |> insert 20 |> insert 15
+  in
   Printf.printf "Set contains 15: %b\n" (member 15 set);
   Printf.printf "Set contains 25: %b\n" (member 25 set);
   Printf.printf "Elements in set: %s\n"
--- bin/rbset/rbset.ml
+++ bin/rbset/rbset.ml
@@ -29,6 +29,30 @@ module Rbset = struct
     | Node (_, a, y, b) -> Node (Black, a, y, b)
     | Empty -> failwith "RBTreeSet: insert failed"
 
+  let remove x s =
+    let rec remove_min = function
+      | Empty -> failwith "RBTreeSet: remove_min failed"
+      | Node (_, Empty, y, right) -> (y, right)
+      | Node (color, left, y, right) ->
+          let min, new_left = remove_min left in
+          (min, balance (color, new_left, y, right))
+    in
+    let rec del = function
+      | Empty -> Empty
+      | Node (color, left, y, right) -> (
+          if x < y then balance (color, del left, y, right)
+          else if x > y then balance (color, left, y, del right)
+          else
+            match right with
+            | Empty -> left
+            | _ ->
+                let min, new_right = remove_min right in
+                balance (color, left, min, new_right))
+    in
+    match del s with
+    | Node (_, a, y, b) -> Node (Black, a, y, b)
+    | Empty -> Empty
+
   let rec to_list = function
     | Empty -> []
     | Node (_, left, x, right) -> to_list left @ (x :: to_list right)
