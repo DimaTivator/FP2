edc86917f2f35eba03d4dea4354dcf259a377406
DimaTivator <andriianov164@gmail.com>
2024-10-09 12:54:55 +0300

    upd: tests and ci

--- test/test_FP2.ml
+++ test/test_FP2.ml
@@ -1,5 +1,15 @@
-open OUnit2
-
 let () =
-  let suite = "MainTestSuite" >::: [ Test_rbset.suite ] in
-  run_test_tt_main suite
+  let open Alcotest in
+  run "Rbset Tests"
+    [
+      ("test_empty", [ test_case "test_empty" `Quick Test_rbset.test_empty ]);
+      ("test_insert", [ test_case "test_insert" `Quick Test_rbset.test_insert ]);
+      ("test_insert_multiple", [ test_case "test_insert_multiple" `Quick Test_rbset.test_insert_multiple ]);
+      ("test_to_list", [ test_case "test_to_list" `Quick Test_rbset.test_to_list ]);
+      ("test_insert_duplicates", [ test_case "test_insert_duplicates" `Quick Test_rbset.test_insert_duplicates ]);
+      ("test_insert_large_numbers", [ test_case "test_insert_large_numbers" `Quick Test_rbset.test_insert_large_numbers ]);
+      ("test_insert_order", [ test_case "test_insert_order" `Quick Test_rbset.test_insert_order ]);
+      ("test_insert_negative_numbers", [ test_case "test_insert_negative_numbers" `Quick Test_rbset.test_insert_negative_numbers ]);
+      ("test_insert_mixed_numbers", [ test_case "test_insert_mixed_numbers" `Quick Test_rbset.test_insert_mixed_numbers ]);
+    ]
+    
--- test/test_rbset.ml
+++ test/test_rbset.ml
@@ -1,51 +1,68 @@
-open OUnit2
+open Alcotest
 open Rbset
 
-let test_empty _ =
-  assert_equal false (Rbset.member 1 Rbset.empty);
-  assert_equal false (Rbset.member 0 Rbset.empty)
+let test_empty () =
+  check bool "empty set does not contain 1" false (Rbset.member 1 Rbset.empty);
+  check bool "empty set does not contain 0" false (Rbset.member 0 Rbset.empty)
 
-let test_insert _ =
+let test_insert () =
   let tree = Rbset.insert 1 Rbset.empty in
-  assert_equal true (Rbset.member 1 tree);
-  assert_equal false (Rbset.member 2 tree)
+  check bool "set contains 1 after insertion" true (Rbset.member 1 tree);
+  check bool "set does not contain 2" false (Rbset.member 2 tree)
 
-let test_insert_multiple _ =
+let test_insert_multiple () =
   let tree = Rbset.insert 3 (Rbset.insert 1 (Rbset.insert 2 Rbset.empty)) in
-  assert_equal true (Rbset.member 1 tree);
-  assert_equal true (Rbset.member 2 tree);
-  assert_equal true (Rbset.member 3 tree);
-  assert_equal false (Rbset.member 4 tree)
+  check bool "set contains 1" true (Rbset.member 1 tree);
+  check bool "set contains 2" true (Rbset.member 2 tree);
+  check bool "set contains 3" true (Rbset.member 3 tree);
+  check bool "set does not contain 4" false (Rbset.member 4 tree)
 
-let test_to_list _ =
+let test_to_list () =
   let tree = Rbset.insert 3 (Rbset.insert 1 (Rbset.insert 2 Rbset.empty)) in
-  assert_equal [ 1; 2; 3 ] (Rbset.to_list tree)
+  check (list int) "to_list returns sorted elements" [ 1; 2; 3 ] (Rbset.to_list tree)
 
-let test_insert_duplicates _ =
+let test_insert_duplicates () =
   let tree = Rbset.insert 1 (Rbset.insert 1 Rbset.empty) in
-  assert_equal true (Rbset.member 1 tree);
-  assert_equal [ 1 ] (Rbset.to_list tree)
+  check bool "set contains 1" true (Rbset.member 1 tree);
+  check (list int) "to_list returns unique elements" [ 1 ] (Rbset.to_list tree)
 
-let test_insert_large_numbers _ =
+let test_insert_large_numbers () =
   let tree = Rbset.insert 1000000 (Rbset.insert (-1000000) Rbset.empty) in
-  assert_equal true (Rbset.member 1000000 tree);
-  assert_equal true (Rbset.member (-1000000) tree);
-  assert_equal false (Rbset.member 0 tree)
+  check bool "set contains 1000000" true (Rbset.member 1000000 tree);
+  check bool "set contains -1000000" true (Rbset.member (-1000000) tree);
+  check bool "set does not contain 0" false (Rbset.member 0 tree)
 
-let test_insert_order _ =
+let test_insert_order () =
   let tree = Rbset.insert 2 (Rbset.insert 1 (Rbset.insert 3 Rbset.empty)) in
-  assert_equal [ 1; 2; 3 ] (Rbset.to_list tree);
+  check (list int) "to_list returns sorted elements" [ 1; 2; 3 ] (Rbset.to_list tree);
   let tree = Rbset.insert 3 (Rbset.insert 2 (Rbset.insert 1 Rbset.empty)) in
-  assert_equal [ 1; 2; 3 ] (Rbset.to_list tree)
+  check (list int) "to_list returns sorted elements" [ 1; 2; 3 ] (Rbset.to_list tree)
 
-let suite =
-  "Rbset Tests"
-  >::: [
-         "test_empty" >:: test_empty;
-         "test_insert" >:: test_insert;
-         "test_insert_multiple" >:: test_insert_multiple;
-         "test_to_list" >:: test_to_list;
-         "test_insert_duplicates" >:: test_insert_duplicates;
-         "test_insert_large_numbers" >:: test_insert_large_numbers;
-         "test_insert_order" >:: test_insert_order;
-       ]
+let test_insert_negative_numbers () =
+  let tree = Rbset.insert (-1) (Rbset.insert (-2) (Rbset.insert (-3) Rbset.empty)) in
+  check bool "set contains -1" true (Rbset.member (-1) tree);
+  check bool "set contains -2" true (Rbset.member (-2) tree);
+  check bool "set contains -3" true (Rbset.member (-3) tree);
+  check bool "set does not contain 0" false (Rbset.member 0 tree)
+
+let test_insert_mixed_numbers () =
+  let tree = Rbset.insert 0 (Rbset.insert (-1) (Rbset.insert 1 Rbset.empty)) in
+  check bool "set contains -1" true (Rbset.member (-1) tree);
+  check bool "set contains 0" true (Rbset.member 0 tree);
+  check bool "set contains 1" true (Rbset.member 1 tree);
+  check bool "set does not contain 2" false (Rbset.member 2 tree)
+
+let () =
+  let open Alcotest in
+  run "Rbset Tests"
+    [
+      ("test_empty", [ test_case "test_empty" `Quick test_empty ]);
+      ("test_insert", [ test_case "test_insert" `Quick test_insert ]);
+      ("test_insert_multiple", [ test_case "test_insert_multiple" `Quick test_insert_multiple ]);
+      ("test_to_list", [ test_case "test_to_list" `Quick test_to_list ]);
+      ("test_insert_duplicates", [ test_case "test_insert_duplicates" `Quick test_insert_duplicates ]);
+      ("test_insert_large_numbers", [ test_case "test_insert_large_numbers" `Quick test_insert_large_numbers ]);
+      ("test_insert_order", [ test_case "test_insert_order" `Quick test_insert_order ]);
+      ("test_insert_negative_numbers", [ test_case "test_insert_negative_numbers" `Quick test_insert_negative_numbers ]);
+      ("test_insert_mixed_numbers", [ test_case "test_insert_mixed_numbers" `Quick test_insert_mixed_numbers ]);
+    ]
